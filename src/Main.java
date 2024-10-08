572d0d9a499f08ee37f6d5bcb2f79fd8b8d021fc
vim <s31600@pjwstk.edu.pl>
2024-10-08 21:21:55 +0200

    Init_1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Adder adder = new Adder();
+            Subtractor  sub = new Subtractor();
+
+        System.out.println(sub.subtract(1,2));
+        System.out.println(adder.add(1,3));
+        }
+    }
